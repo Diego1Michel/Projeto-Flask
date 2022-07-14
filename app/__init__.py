d2b7295787a517a2afb41a35e19338ecf53e330b
Diego1Michel <diego.michel859@gmail.com>
2022-07-14 20:01:20 -0300

    master

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, flash, redirect, render_template, request
 app = Flask(__name__) #instanciando classe flask
 
 #endereço do site
@@ -14,6 +14,35 @@ def index():
 @app.route('/Contato') #rota página contato
 def contato(): #define função contato
     return render_template('Contato.html')
+
+@app.route('/produtos') #rota página contato
+def produtos(): #define função contato
+    return render_template('produtos.html')
+
+@app.route('/login') #rota página contato
+def login(): #define função contato
+    return render_template('login.html')
+
+# @app.route('/autenticar' , method=['GET']) #rota página contato
+# def autenticar(): #define função contato
+#     usuario = request.args.get('usuario')
+#     senha = request.args.get('senha')
+#     return "usuario:{} e senha:{}" .format(usuario,senha)
+
+# chave secreta para poder realizar o redirect da página
+app.config['SECRET_KEY'] = "minha-chave"
+
+
+@app.route('/autenticar', methods=['POST']) #rota página contato
+def autenticar(): #define função contato
+    usuario = request.form.get('usuario') #enviar form
+    senha = request.form.get('senha') 
+    if (usuario=='admin' and senha=='12345'):
+        return "usuario:{} e senha:{}" .format(usuario,senha)   
+    else:
+        flash('Dados Inválidos!')
+        return redirect('/login')
+    
 
 
 app.run()
